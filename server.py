613ebbc1504371cdc89b14e992a02ea61215afa3
Chengfeng Ren <chengfengren@gmail.com>
2022-03-29 04:23:02 +0000

    flask-intro

---
+++ server.py
@@ -0,0 +1,93 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+DISS = ["high-ball","low-ball"]
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Start Here</title>
+      </head>
+      <body>
+        <a href="/hello">Take me to the start</a>
+      </body>
+    </html>
+    """
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person"><br>
+          <label>Select a compliment</label>
+          <select name="compliment">
+              <option value="awesome">awesome</option>
+              <option value="terrific">terrific</option>
+          </select>
+        <label>Select a diss</label>
+        <select name="diss">
+            <option value="too-high">too-high</option>
+            <option value="drunk">drunk</option>
+        </select>
+        <input type="submit" value="Submit"><br>
+        </form>
+
+      </body>
+    </html>
+    """
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    # compliment = choice(AWESOMENESS)
+    compliment = request.args.get('compliment')
+    diss = request.args.get('diss')
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {compliment}, but not {diss}!
+      </body>
+    </html>
+    """
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0")
